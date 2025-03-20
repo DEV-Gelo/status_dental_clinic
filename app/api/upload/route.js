893be633fb5a081c72f0b357cc73c7408bcfb34f
DEV-Gelo <chichkoff@gmail.com>
2025-03-20 15:39:18 +0200

    integration_storage

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,5 +1,6 @@
 // ----Uploading a file to the storage in supabase----//
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 import { supabase } from "@/lib/supabase";
 
 export async function POST(req) {
