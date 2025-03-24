e9ddb5e013e1a980f60aa7eed8376ad78d60cfa2
DEV-Gelo <chichkoff@gmail.com>
2025-03-24 13:14:19 +0200

    fixed_calendar

--- components/Schedule/Calendar/Calendar.jsx
+++ components/Schedule/Calendar/Calendar.jsx
@@ -21,9 +21,11 @@ export default function Calendar({
   const [errorMessage, setErrorMessage] = useState("");
   const [slots, setSlots] = useState([]);
 
+  // ---------Translations-------------//
   const t = useTranslations("schedule__calendar");
   const local = t("language");
 
+  // ---------- Load slots for the selected doctor------------//
   useEffect(() => {
     if (selectedDoctor) {
       setSelectedDates(resetSelectedDates);
@@ -32,7 +34,9 @@ export default function Calendar({
         .then((response) => response.json())
         .then((data) => {
           const groupedSlots = data.reduce((acc, slot) => {
-            const scheduleDate = new Date(slot.schedule.date).toISOString();
+            const scheduleDate = new Date(slot.schedule.date)
+              .toISOString()
+              .split("T")[0];
             if (!acc[scheduleDate]) {
               acc[scheduleDate] = [];
             }
@@ -40,8 +44,9 @@ export default function Calendar({
             return acc;
           }, {});
 
+          // ------Transfer grouped slots by date-----------//
           onDaySchedule(groupedSlots);
-
+          // ---Select the first and last slot for each date----//
           const firstAndLastSlots = Object.keys(groupedSlots).map((date) => {
             const slotsOnDate = groupedSlots[date];
             return {
@@ -50,48 +55,59 @@ export default function Calendar({
             };
           });
 
-          setSlots(firstAndLastSlots);
+          setSlots(firstAndLastSlots); // Update the slots state
           isLoadingSchedule(false);
         })
         .catch((error) => {
           console.error("Error loading slots:", error);
-          setSlots([]);
+          setSlots([]); // Clear slots if error occurs
         });
     } else {
-      setSlots([]);
+      setSlots([]); // Clear slots if no doctor is selected
     }
   }, [selectedDoctor, resetSelectedDates]);
 
+  // -----Reset error message after select doctor--------//
   useEffect(() => {
     if (selectedDoctor && errorMessage) {
-      setErrorMessage("");
+      setErrorMessage(""); // Reset error message if doctor is selected
     }
   }, [selectedDoctor]);
 
+  // -----Send date--------//
   useEffect(() => {
-    onTransferableDate(currentDate);
+    onTransferableDate(currentDate); // Pass the current date to parent component
   }, [currentDate]);
 
   const getMonthDays = (year, month) => new Date(year, month + 1, 0).getDate();
 
+  // ------Set Monday as the first day of the week-----//
   const getStartDayOfMonth = (year, month) => {
     const day = new Date(year, month, 1).getDay();
     return day === 0 ? 6 : day - 1;
   };
 
-  const prevMonth = () =>
+  // ------------Switch month to prev------------//
+  const prevMonth = () => {
     setCurrentDate(
       new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
     );
-  const nextMonth = () =>
+    setErrorMessage(""); // Reset error message when the month changes
+  };
+
+  // ------------Switch month to next------------//
+  const nextMonth = () => {
     setCurrentDate(
       new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
     );
+    setErrorMessage(""); // Reset error message when the month changes
+  };
 
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
   const daysInMonth = getMonthDays(year, month);
   const startDay = getStartDayOfMonth(year, month);
+
   const daysArray = Array.from({ length: 42 }, (_, i) => {
     const day = i - startDay + 1;
     return day > 0 && day <= daysInMonth ? day : "";
@@ -105,24 +121,43 @@ export default function Calendar({
   const todayMonth = today.getMonth();
   const todayYear = today.getFullYear();
 
+  // -----------Select a day in the calendar-----------//
   const handleDayClick = (day) => {
-    if (!day || !selectedDoctor) {
-      onAlert("warning", t("notSelectedDoctor"));
+    if (!day) return; // Exit if no day is selected
+
+    if (!selectedDoctor) {
+      onAlert("warning", t("notSelectedDoctor")); // Alert if no doctor is selected
       return;
     }
+    // -------------Select dates---------------//
+    if (
+      day &&
+      (new Date(year, month, day) > today ||
+        (year === todayYear && month === todayMonth && day === todayDay))
+    ) {
+      // Format the selected date in UTC
+      const newSelectedDate = new Date(
+        Date.UTC(year, month, day)
+      ).toISOString(); // Convert date to UTC and format as ISO string
 
-    const newSelectedDate = new Date(Date.UTC(year, month, day)).toISOString();
-    const isSelected = selectedDates.includes(newSelectedDate);
+      // Check if the date is already selected
+      const isSelected = selectedDates.includes(newSelectedDate);
 
-    let updatedDates = isSelected
-      ? selectedDates.filter((dateStr) => dateStr !== newSelectedDate)
-      : [...selectedDates, newSelectedDate];
+      let updatedDates;
+      if (isSelected) {
+        updatedDates = selectedDates.filter(
+          (dateStr) => dateStr !== newSelectedDate // Remove date if already selected
+        );
+      } else {
+        updatedDates = [...selectedDates, newSelectedDate]; // Add date if not selected
+      }
 
-    console.log("updatedDates:", updatedDates);
+      console.log("updatedDates :", updatedDates);
 
-    if (JSON.stringify(updatedDates) !== JSON.stringify(selectedDates)) {
-      setSelectedDates(updatedDates);
-      onDateSelect(updatedDates);
+      if (JSON.stringify(updatedDates) !== JSON.stringify(selectedDates)) {
+        setSelectedDates(updatedDates); // Update selected dates state
+        onDateSelect(updatedDates); // Pass selected dates to parent component
+      }
     }
   };
 
@@ -154,29 +189,46 @@ export default function Calendar({
           </div>
         ))}
         {daysArray.map((day, i) => {
-          const dayISO = day
-            ? new Date(Date.UTC(year, month, day)).toISOString()
-            : null;
-          const isSelected = selectedDates.includes(dayISO);
-          const slotsForDay = slots.find(
-            (slotGroup) => slotGroup.date === dayISO
+          const isPastDate =
+            day &&
+            new Date(year, month, day) <
+              new Date(todayYear, todayMonth, todayDay);
+          const isToday =
+            day === todayDay &&
+            month === todayMonth &&
+            year === today.getFullYear();
+          const isSelected = selectedDates.includes(
+            new Date(Date.UTC(year, month, day)).toISOString()
           );
+          //  Filter only those slots that correspond to the current day
+          const slotsForDay =
+            day &&
+            slots.find(
+              (slotGroup) =>
+                slotGroup.date ===
+                new Date(Date.UTC(year, month, day)).toISOString().split("T")[0]
+            );
 
           return (
             <div
               key={i}
-              className={`${styles.day} ${isSelected ? styles.selected : ""} ${
+              className={`${styles.day} ${isToday ? styles.today : ""} ${
+                isSelected ? styles.selected : ""
+              } ${isPastDate ? styles.disabled : ""} ${
                 day ? styles.day : styles.emptyDay
               }`}
-              onClick={() => day && handleDayClick(day)}
+              onClick={() => !isPastDate && handleDayClick(day)}
             >
               {day}
-              {day && slotsForDay && slotsForDay.slots.length > 0 && (
-                <span className={styles.slot}>
-                  {slotsForDay.slots[0].time} -{" "}
-                  {slotsForDay.slots[slotsForDay.slots.length - 1].time}
-                </span>
-              )}
+              {day &&
+                !isPastDate &&
+                slotsForDay &&
+                slotsForDay.slots.length > 0 && (
+                  <span className={styles.slot}>
+                    {slotsForDay.slots[0].time} -{" "}
+                    {slotsForDay.slots[slotsForDay.slots.length - 1].time}
+                  </span>
+                )}
             </div>
           );
         })}
