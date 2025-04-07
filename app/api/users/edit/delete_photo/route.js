c8d2e05a3534d610af205b29c9c783614c8458a6
DEV-Gelo <chichkoff@gmail.com>
2025-04-07 16:52:53 +0300

    fixed

--- app/api/users/edit/delete_photo/route.js
+++ app/api/users/edit/delete_photo/route.js
@@ -1,37 +1,37 @@
-import { NextResponse } from "next/server";
-import { supabase } from "@/lib/supabase";
+// import { NextResponse } from "next/server";
+// import { supabase } from "@/lib/supabase";
 
-// ----- API to delete previous photo when resetting photo via toggle ----//
+// // ----- API to delete previous photo when resetting photo via toggle ----//
 
-export async function POST(req) {
-  try {
-    const { photoUrl } = await req.json();
-    // Trim part of the URL to get only the path to the file in the bucket
-    const fileOldPath = photoUrl
-      .replace("blob:https://", "blob:https:/") // Remove the extra "/"
-      .split("supabase.co/storage/v1/object/public/uploads")[1] // Extract the part after "uploads/"
-      ?.replace(/^\/+/, ""); //Remove extra slashes at the beginning
-    // Delete previous file
-    const { data, error } = await supabase.storage
-      .from("uploads")
-      .remove([fileOldPath]);
+// export async function POST(req) {
+//   try {
+//     const { photoUrl } = await req.json();
+//     // Trim part of the URL to get only the path to the file in the bucket
+//     const fileOldPath = photoUrl
+//       .replace("blob:https://", "blob:https:/") // Remove the extra "/"
+//       .split("supabase.co/storage/v1/object/public/uploads")[1] // Extract the part after "uploads/"
+//       ?.replace(/^\/+/, ""); //Remove extra slashes at the beginning
+//     // Delete previous file
+//     const { data, error } = await supabase.storage
+//       .from("uploads")
+//       .remove([fileOldPath]);
 
-    if (error) {
-      console.error("Error deleting file:", error);
-      return NextResponse.json(
-        { message: "Failed to delete file" },
-        { status: 500 }
-      );
-    }
-    return NextResponse.json(
-      { message: "File deleted successfully" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error deleting photo:", error);
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  }
-}
+//     if (error) {
+//       console.error("Error deleting file:", error);
+//       return NextResponse.json(
+//         { message: "Failed to delete file" },
+//         { status: 500 }
+//       );
+//     }
+//     return NextResponse.json(
+//       { message: "File deleted successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error deleting photo:", error);
+//     return NextResponse.json(
+//       { message: "Internal server error" },
+//       { status: 500 }
+//     );
+//   }
+// }
