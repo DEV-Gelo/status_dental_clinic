96b3d55c74e89d2a537a570bb59b2d76111c7849
DEV-Gelo <chichkoff@gmail.com>
2025-07-25 17:30:21 +0300

    test

--- components/AdminNav/AdminNav.jsx
+++ components/AdminNav/AdminNav.jsx
@@ -10,6 +10,8 @@ import { RiPagesLine } from "react-icons/ri";
 import { RiMoneyDollarBoxLine } from "react-icons/ri";
 import { SlNote } from "react-icons/sl";
 
+const num = 0;
+
 const AdminNav = () => {
   const pathname = usePathname();
   const locale = pathname.split("/")[1];
